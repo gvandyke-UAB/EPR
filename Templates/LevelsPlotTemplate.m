c041d699d6f11ebc6b1208d0268d80426f671d39
Garrett Van Dyke <gvd417@gmail.com>
2020-03-11 14:47:11 -0500

    comments

--- Templates/LevelsPlotTemplate.m
+++ Templates/LevelsPlotTemplate.m
@@ -30,7 +30,7 @@ levelsplot(Sys,'z',FieldRange,Freq,Exp);
 
 
 % get eigenvalues and eigenvectors to compare with our own script
-% "EasySpinReplication.m"
+% "Diagonalize.m"
 for i = 1:1001
     
     B_0 = [0, 0, i-1]; % static magnetic field in mT

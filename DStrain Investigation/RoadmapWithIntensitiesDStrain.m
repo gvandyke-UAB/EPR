ec38c199678102bb3f8c3f2d7f2c521f02a39539
Garrett Van Dyke <gvd417@gmail.com>
2020-04-02 20:26:50 -0400

    DStrain Investigations

--- DStrain Investigation/RoadmapWithIntensitiesDStrain.m
+++ DStrain Investigation/RoadmapWithIntensitiesDStrain.m
@@ -7,10 +7,10 @@ clear all;
 clf % clears all variables and figures
 
 % Name your EPR centers here, used for plot formatting later
-center1 = 'Cr';
-center2 = 'Fe3+';
+center1 = 'Cr1';
+center2 = 'Cr2';
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Cr lines %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Cr1 lines %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%% Generate rotations about nL %%%%%%%%%%
 nL = [1;0;0]; % rotating about mW magnetic field
@@ -24,6 +24,7 @@ cori = rotatecrystal(cori0,nL,rho);
 Sys.S = 3/2;
 Sys.g = [1.968 0 -0.008; 0 1.964 0; 0 0 1.973];
 Sys.D = [3*5385 3*1288];
+Sys.DStrain = [1000 1200]; % values are for experimenting and do not reflect a sample at this time
 %================================%
 
 
@@ -42,70 +43,69 @@ Exp.CrystalOrientation = cori;
 
 
 %%%%%%%%%% Generate B field roadmap data %%%%%%%%%%
-[BresCr, IntCr, WidCr] = resfields(Sys,Exp,Opt);
-angCr = rho * 180/pi - 90;
+[BresCr1, IntCr1, WidCr1] = resfields(Sys,Exp,Opt);
+angCr1 = rho * 180/pi - 90;
 %================================%
 
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Fe3+ lines %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Cr2 lines %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Octahedral
 
 %%%%%%%%%% Generate rotations about nL %%%%%%%%%%
 nL = [1;0;0]; % rotating about mW magnetic field
-cori0 = [0 102 0] * pi/180; % align vertical of sample with mw Field
-rho = (90:3:270) * pi/180; % 90 deg is 0 deg in our expt ie B//b
+cori0 = [0 102 0] * pi/180; 
+rho = (90:2:270) * pi/180;
 cori = rotatecrystal(cori0,nL,rho);
 %================================%
 
 
 %%%%%%%%%% Spin parameters %%%%%%%%%%
-Sys.S = 5/2;
-Sys.g = [2.004 2.002 2.007];
+Sys.S = 3/2;
+Sys.g = [1.968 0 -0.008; 0 1.964 0; 0 0 1.973];
 Sys.D = [3*5385 3*1288];
-Sys.DStrain = [10 12]; % values are for experimenting and do not reflect sample at this time
-% reproduces RT angle dependence of Ga2O3:Mg doped sample well except for
-% relative line intensities
+Sys.DStrain = [10 12]; % values are for experimenting and do not reflect a sample at this time
 %================================%
 
 
-%%%%%%%%%% Optional Parameters %%%%%%%%%%
-Opt.Output = 'separate';  % make sure spectra are not added up
+%%%%%%%%%% Optional parameters %%%%%%%%%%
+Opt.Output = 'separate'; % make sure spectra are not added up
 %================================%
 
 
 %%%%%%%%%% Experimental parameters %%%%%%%%%%
-Exp.Temperature = 298; 
 Exp.mwFreq = 9.4066;
 Exp.Range = [50 1100];
-Exp.CrystalSymmetry = 'C2/m';  % assumes 'b' is yC
-Exp.nPoints = 1e4;
+Exp.CrystalSymmetry = 'C2/m';  %assumes 'b' is yC
+Exp.Temperature = 298;
 Exp.CrystalOrientation = cori;
 %================================%
 
 
 %%%%%%%%%% Generate B field roadmap data %%%%%%%%%%
-[BresFe, IntFe, WidFe] = resfields(Sys,Exp,Opt);
-angFe = rho * 180/pi - 90;
+[BresCr2, IntCr2, WidCr2] = resfields(Sys,Exp,Opt);
+angCr2 = rho * 180/pi - 90;
 %================================%
 
-
+%{
 %%%%%%%%%% Overall Normalization %%%%%%%%%%
-setToOne = max(max(IntFe,[],'all'), max(IntCr,[],'all')); % find which value to regard as our "1"
-normalizedIntCr = IntCr / setToOne;
-normalizedIntFe = IntFe / setToOne;
+setToOne = max(max(IntCr1,[],'all'), max(IntCr2,[],'all')); % find which value to regard as our "1"
+normalizedIntCr1 = IntCr1 / setToOne;
+normalizedIntCr2 = IntCr2 / setToOne;
 %================================%
+%}
 
-
+%{
 %%%%%%%%%% Plotting %%%%%%%%%%
 set(gcf, 'Name','Roadmap + Intensities EasySpin Simulation','numbertitle','off');
-plot3(BresCr*10, angCr, normalizedIntCr,'k','linewidth',2,'DisplayName',center1); % black traces
+plot3(BresCr1*10, angCr1, normalizedIntCr1,'k','linewidth',2,'DisplayName',center1); % black traces
 hold on;
-plot3(BresFe*10, angFe, normalizedIntFe,'b','linewidth',2,'DisplayName',center2); % blue traces
+plot3(BresCr2*10, angCr2, normalizedIntCr2,'b','linewidth',2,'DisplayName',center2); % blue traces
 legend
-title(strcat('Roadmap + Intensities for',{' '}, center1,{' '}, 'and',{' '}, center2));
+title(strcat('Roadmap + Intensities for',{' '},center1,{' '},'and',{' '},center2));
 xlabel('Magnetic Field (mT)');
 ylabel('Angle of rotation (°)');
 zlabel('Relative Intensity (a.u.)');
 %================================%
+%}
---
+++ DStrain Investigation/SpectraDStrain.m
@@ -0,0 +1,53 @@
+clear all;
+
+%%%%%%%%%%%%%%%%%%%% Fe3+ %%%%%%%%%%%%%%%%%%%%
+
+center1 = 'Fe3+'; % name your EPR center for plotting
+
+% Octahedral
+
+%%%%%%%%%% Generate rotations about nL %%%%%%%%%%
+nL = [1;0;0]; % rotating about mW magnetic field
+cori0 = [0 102 0]*pi/180; % align vertical of sample with mw Field
+rho = (90)*pi/180; % 90 deg is 0 deg in our expt ie B//b
+cori = rotatecrystal(cori0,nL,rho);
+%================================%
+
+
+%%%%%%%%%% Spin parameters %%%%%%%%%%
+Sys.S = 5/2;
+Sys.g = [2.004 2.002 2.007];
+Sys.lwpp = 1.6;
+Sys.DStrain = [100 120];
+%================================%
+
+
+%%%%%%%%%% Experimental parameters %%%%%%%%%%
+Exp.Temperature = 298; 
+Exp.mwFreq = 9.4066;
+Exp.Range = [50 1100];
+Exp.CrystalSymmetry = 'C2/m';  %assumes 'b' is yC
+Exp.nPoints = 1e4;
+Exp.CrystalOrientation = cori;
+%================================%
+
+
+%%%%%%%%%% Optional parameters %%%%%%%%%%
+Opt.Output = 'separate';  % make sure spectra are not added up
+%================================%
+
+
+%%%%%%%%%% Simulate spectra %%%%%%%%%%
+pepper(Sys,Exp,Opt); % this pepper call plots
+
+set(gcf, 'Name','Spectrum EasySpin Simulation','numbertitle','off');
+title(strcat(center1,{' '},'Spectrum'));
+
+[B,spec] = pepper(Sys,Exp,Opt); % this pepper call stores values so we can write them to a .txt file
+%================================%
+
+
+%%%%%%%%%% Save data to .txt %%%%%%%%%%
+data = [B(:)*10 spec(:)*8000]; % B(:)*10 converts from mT to G
+save('GaOFespc_90D.txt','data','-ascii');
+%================================%
